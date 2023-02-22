daffd8b99b2b27417e588f863abbaa7f6920b914
Umair Shehzad <umairshehzzad247365@gmail.com>
2023-02-22 20:21:21 +0500

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  PORT: process.env.PORT,
+  MONGODB_URL: process.env.MONGODB_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+};
